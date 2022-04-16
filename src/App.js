849cf798a4588499817a91b9fa63fb1a20075639
NeelPoros <sawkotalimahmud@gmail.com>
2022-04-16 10:45:36 -0700

    Footer Section Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Footer></Footer>
     </div>
   );
 }
