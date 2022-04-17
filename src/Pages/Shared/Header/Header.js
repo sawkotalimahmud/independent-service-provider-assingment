e1857b6c69fbec6e8f2c1675f01d527aed0fff70
NeelPoros <sawkotalimahmud@gmail.com>
2022-04-17 04:43:54 -0700

    Header Section Is Ready

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,6 +4,11 @@ const Header = () => {
     return (
         <div>
             <h2 className='text-3xl'>This Is Header</h2>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/login">Login</a>
+            </nav>
         </div>
     );
 };
