aa3174bd5a315c072345e46557d666b751b3c811
NeelPoros <sawkotalimahmud@gmail.com>
2022-04-17 03:22:02 -0700

    new project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import About from "./Pages/About/About";
-import Home from "./Pages/Home/Home/Home";
-import Footer from "./Pages/Shared/Footer/Footer";
-import Header from "./Pages/Shared/Header/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-      </Routes>
-      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,21 +1,11 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React from 'react';
 
 const Header = () => {
-  return (
-    <div className="fixed-top">
-      <Navbar bg="primary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Plumber David</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </div>
-  );
+    return (
+        <div>
+            <h2 className='text-3xl'>This Is Header</h2>
+        </div>
+    );
 };
 
 export default Header;
