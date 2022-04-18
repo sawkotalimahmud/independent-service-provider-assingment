604df450f951a85b1ff305124c389e6232368f84
NeelPoros <sawkotalimahmud@gmail.com>
2022-04-18 01:32:24 -0700

    Sign Out Button Added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About/About';
-import Blog from './Pages/Blog/Blog';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login/Login';
-import SignUp from './Pages/Login/SignUp/SignUp';
-import PageNotFound from './Pages/PageNotFound/PageNotFound';
-import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
-import Footer from './Pages/Shared/Footer/Footer';
-import Header from './Pages/Shared/Header/Header';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Blog from "./Pages/Blog/Blog";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import SignUp from "./Pages/Login/SignUp/SignUp";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+import RequireAuth from "./Pages/RequireAuth/RequireAuth";
+import ServiceDetail from "./Pages/ServiceDetail/ServiceDetail";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/service' element={<ServiceDetail></ServiceDetail>}>
-          <Route path=':serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
-        </Route>
-        <Route path='/blog' element={<Blog></Blog>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/signup' element={<SignUp></SignUp>}></Route>
-        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route
+          path="/service/:serviceId"
+          element={
+            <RequireAuth>
+              <ServiceDetail></ServiceDetail>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,17 +1,31 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 import CustomLink from '../../CustomLink/CustomLink';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut =()=>{
+        signOut(auth)
+    }
+
     return (
         <div className='flex justify-between sticky'>
-            <nav className='gap-10 text-2xl font-medium	m-6'>
+            <nav className='gap-5 text-2xl font-medium	m-4'>
             <CustomLink to="/">Plumber David</CustomLink>
             </nav>
-            <nav className='flex justify-center gap-10 text-2xl font-medium	m-6'>
+            <nav className='flex justify-center gap-5 text-2xl font-medium	m-4'>
                 <CustomLink to="/">Home</CustomLink>
                 <CustomLink to="/about">About</CustomLink>
                 <CustomLink to="/blog">Blog</CustomLink>
-                <CustomLink to="/login">Login</CustomLink>
+                {
+                    user ?
+                    <CustomLink to={"/"} onClick={handleSignOut} >Sing Out</CustomLink>
+                    :
+                    <CustomLink to="/login">Login</CustomLink>
+                }
             </nav>           
         </div>
     );
